d98e9cd60102847bd16f00b6e778acf59ee88fbb
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-14 16:56:16 +0300

    task3js

---
+++ controlStructures.js
@@ -0,0 +1,43 @@
+
+
+
+let num1=15;
+let num2=10;
+let num3=5;
+
+
+
+if(num1 > num2)
+{
+console.log(num1);
+}
+else {
+if(num1 > num2)
+console.log(num2);
+}
+
+/*  odd or even */
+
+if(num1 % 2 == 0) {
+    console.log("The number is even");
+}
+
+else {
+    console.log("The number is odd");
+}
+
+/*largest to smallest */
+if (num1>num2 && num1>num3)
+{
+        if (num2>num3)
+        {
+            console.log(num1 + ", " + num2 + ", " +num3);
+        }
+        else
+        {
+            console.log(num1 + ", " + num3+ ", " +num2);
+        }
+}
+
+
+/*  */
