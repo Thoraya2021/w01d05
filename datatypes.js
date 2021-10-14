ed04a364c400164a1f705fae5043becf089d309d
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-14 15:18:09 +0300

    task2js

--- datatypes.js
+++ datatypes.js
@@ -5,11 +5,18 @@ console.log(x);
 console.log(language);
 let names = ["thoraya", "shabab", "rashidi"];
 console.log(names);
-
-
-
+Array
 console.log(typeof x); 
 console.log(typeof language); 
 console.log(typeof names); 
 
+let information = ["thoraya", "qassim", 24];
+console.log(information);
+console.log(typeof information); 
+
+/* condition */
+
+let str ="heLLO world"
+
+let status="upper"
 
