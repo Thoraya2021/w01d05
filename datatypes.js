a50f0771ae0827f84f0fd3cdc2aa39a0371c2517
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-14 13:02:59 +0300

    task2js

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let x =10;
+let language ="javascript";
+
+console.log(x);
+console.log(language);
+let names = ["thoraya", "shabab", "rashidi"];
+console.log(names);
+
+
+
+console.log(typeof x); 
+console.log(typeof language); 
+console.log(typeof names); 
+
+
