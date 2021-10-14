9cdcbc784aee5cf2f4fc8708b191538e5102d3a3
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-14 11:52:47 +0300

    task js 1

---
+++ myvariables.js
@@ -0,0 +1,20 @@
+let language ="javascript" ;
+let score=10;
+console.log(language + score);
+
+
+
+let length=10;
+let width=5;
+console.log(`remember area= ${length*width}`);
+
+
+
+let num1=20;
+let num2=5;
+console.log(num1/num2);
+
+
+
+
+
